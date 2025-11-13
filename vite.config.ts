d9b70210baeea70a235901f3807e051190a8aab8
Giovanni Abbatepaolo <30571828+bbtgnn@users.noreply.github.com>
2025-11-13 12:36:16 +0100

    feat: scoped tailwind

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,22 @@ import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
 import path from 'path';
+import prefixer from 'postcss-prefix-selector';
+import autoprefixer from 'autoprefixer';
 
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [tailwindcss(), svelte()],
+	css: {
+		postcss: {
+			plugins: [
+				prefixer({
+					prefix: '#join-calendar'
+				}),
+				autoprefixer({})
+			]
+		}
+	},
 	resolve: {
 		alias: {
 			$lib: path.resolve('./src/lib')
